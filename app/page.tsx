2e28398f9bfcc50fec056240d1d488ee7f7218fb
Kevin Guo <kguo28@berkeley.edu>
2025-10-11 17:14:13 -0700

    Accordion

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import Image from "next/image";
 import {toast} from "sonner";
 import { TextHoverEffect } from "@/components/ui/text-hover-effect";
+import { Accordion, AccordionItem, AccordionTrigger, AccordionContent} from "@/components/ui/accordion";
 
 export default function Home() {
   return (
@@ -37,15 +38,52 @@ export default function Home() {
               Hi, I’m Kevin Guo! I . . .
               </CardTitle>
               <CardContent>
-              <p className = "text-lg">
-                - Am a UC Berkeley M.E.T. student (EECS + Business Administration) building AI systems that turn data into real-world impact. 
-              </p>
-              <p className = "text-lg">
-                - Have worked across defense tech, venture intelligence, and humanitarian AI, from drone detection pipelines to real-time voice agents and climate analytics platforms. 
-              </p>
-              <p className = "text-lg">
-                - Love creating systems that are technically rigorous, ethically grounded, and operational at scale.
-              </p>
+                <Accordion
+                  type = "single"
+                  collapsible
+                >
+                  <AccordionItem value="item-1">
+                    <AccordionTrigger className = "text-4xl">
+                      Student at UC Berkeley
+                    </AccordionTrigger>
+                    <AccordionContent>
+                      <p className = "text-lg">I currently study EECS + Business Administration as part of the M.E.T. Program! I love building AI systems that turn data into real-world impact.</p>
+                      <br/>
+                      <p className = "text-lg">I have worked across defense tech, venture intelligence, and humanitarian AI, from drone detection pipelines to real-time voice agents and climate analytics platforms.</p>
+                    </AccordionContent>
+                  </AccordionItem>
+
+                  <AccordionItem value="item-2">
+                    <AccordionTrigger className = "text-4xl">
+                      From Michigan!
+                    </AccordionTrigger>
+                    <AccordionContent>
+                      <p className = "text-lg">I was born in Ohio, but raised in Rochester Hills, Michigan. I moved to the Bay Area in 2024 for the start of college. </p>
+                    </AccordionContent>
+                  </AccordionItem>
+                  
+
+                  <AccordionItem value="item-3">
+                    <AccordionTrigger className = "text-4xl">
+                      Risk Taker
+                    </AccordionTrigger>
+                    <AccordionContent>
+                      <p className = "text-lg">I’ve always been comfortable taking calculated risks — whether it’s diving into an unfamiliar technology, leading a project outside my comfort zone, or pitching unconventional ideas.</p>
+                      <br/>
+                      <p className = "text-lg">I love the thrill of building something new and seeing how far I can push my limits. </p>
+                    </AccordionContent>
+                  </AccordionItem>
+
+                  <AccordionItem value="item-4">
+                    <AccordionTrigger className = "text-4xl">
+                      Entrepreneur
+                    </AccordionTrigger>
+                    <AccordionContent>
+                      <p className = "text-lg">I was born in Ohio, but raised in Rochester Hills, Michigan. I moved to the Bay Area in 2024 for the start of college. </p>
+                    </AccordionContent>
+                  </AccordionItem>
+                </Accordion>
+  
               </CardContent>
             </Card>
           </div>
